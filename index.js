f3ef57e1e250f45bf721853d7a6cb2f2cf8b693a
CRNaro <144285637+CRNaro@users.noreply.github.com>
2023-12-09 13:48:11 -0500

    Build/Fix: Generator is functioning, just need to check other shapes and get the logo to open in a browser

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//require = require("esm")(module/*, options*/);   
+// This file is the entry point for the application. It uses inquirer to prompt the user for input and then generates an SVG file based on the user's input. 
 const inquirer = require('inquirer');
 const fs = require('fs');               
-//const {logoPrompts} = require('./lib/prompts.mjs');    //, textColorPrompts, shapePrompts, shapeColorPrompts
 const { logoPrompts } = require ("./lib/prompts.js");
+const { Triangle, Circle, Square } = require('./lib/shapes.js');
 
 (async() => {
     //const {default: inquirer} = await import('inquirer');
@@ -10,13 +10,27 @@ const { logoPrompts } = require ("./lib/prompts.js");
    
     inquirer.prompt(logoPrompts)     //, textColorPrompts, shapePrompts, shapeColorPrompts
     .then(answers => {
-        const svg = `<svg xmlns="http://www.w3.org/2000/svg">
-            <text x="10" y="50" fill="${answers.textColor}">${answers.text}</text>
-            <${answers.shape} cx="50" cy="50" r="40" fill="${answers.shapeColor}" />
+       let shape = '';
+        switch(answers.shape) {
+            case 'Circle':
+                shape = `<circle cx="50" cy="50" r="40" fill = "${answers.shapeColor}" />`;
+                break;
+            case 'Triangle':
+                shape = `<polygon points = "50,15 100,100 0,100" fill = "${answers.shapeColor}" />`;
+                break;
+            case 'Square':
+                shape = `<rect x="100" y="100" width="70" height="70" fill = "${answers.shapeColor}" />`;
+        }
+
+        const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+            ${shape}
+            <text x="50" y="55" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
             </svg>`;
 
-        fs.writeFile('image.svg', svg, (err) => {
-            if (err) throw err;
+        fs.writeFile('./examples/logo.svg', svg, (err) => {
+            if (err) {
+                console.log(err);
+            }
             console.log('The file has been saved!');
         });
     })
