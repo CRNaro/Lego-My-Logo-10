fc19b429f3a0a1001066b230b50b189f3a4b9465
CRNaro <144285637+CRNaro@users.noreply.github.com>
2023-12-09 11:57:15 -0500

    Build/Fix: Added SVG properly, and fixed code to prompt user properly choose and create an image.svg file.  Need to make it so the text falls inside the shape and the file can be opened in browser next

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+//require = require("esm")(module/*, options*/);   
 const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require('fs');               
+//const {logoPrompts} = require('./lib/prompts.mjs');    //, textColorPrompts, shapePrompts, shapeColorPrompts
+const { logoPrompts } = require ("./lib/prompts.js");
+
+(async() => {
+    //const {default: inquirer} = await import('inquirer');
+    //const {logoPrompts} = await import('./lib/prompts.js');    //, textColorPrompts, shapePrompts, shapeColorPrompts
+   
+    inquirer.prompt(logoPrompts)     //, textColorPrompts, shapePrompts, shapeColorPrompts
+    .then(answers => {
+        const svg = `<svg xmlns="http://www.w3.org/2000/svg">
+            <text x="10" y="50" fill="${answers.textColor}">${answers.text}</text>
+            <${answers.shape} cx="50" cy="50" r="40" fill="${answers.shapeColor}" />
+            </svg>`;
+
+        fs.writeFile('image.svg', svg, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    })
+    .catch(error => {
+        if(error.isError) {
+            console.log("Prompt couldn't be rendered in the current environment");
+        }
+    });
+})();
+//module.exports = {
+    //logoPrompts
+//}
